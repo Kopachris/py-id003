5e377428cc14b88d482da61c3ab490a7f7bfdba8
Christopher Koch <kopachris@gmail.com>
2015-12-04 18:53:48 +0000

    clean up test code a little

--- test.py
+++ test.py
@@ -8,42 +8,23 @@ import time
 
 
 def main():
-    #avail_ports = list(serial.tools.list_ports.comports())
-    #if not len(avail_ports):
-    #    raise Exception("No serial port available")
-    #else:
-    #    com_port = -1
-    #    while -1 < com_port < len(avail_ports):
-    #        for i, p in enumerate(avail_ports):
-    #            print(i + ') ' + p)
-    #        com_port = int(input("Which com port? "))
-    #    port = avail_ports[com_port]
-        
-    baud = None
-    while baud is None:
-        baud = int(input("Baud rate? (Only 9600 and 19200 supported) "))
     
     timeout = 0.2
+    baud = 9600
+    port = 'COM11'  # JCM UAC device (USB serial adapter)
     
-    bv = BillVal('COM11', baud, serial.EIGHTBITS, serial.PARITY_EVEN, timeout=timeout)
-    #bv.send_command(id003.RESET)
-    #bv.power_on()
-    #bv.req_status()
+    bv = BillVal(port, baud, serial.EIGHTBITS, serial.PARITY_EVEN, timeout=timeout)
+    bv.power_on()
     
-    #if bv.init_status == id003.POW_UP:
-    #    print("BV powered up normally, version:\n\t" + bv.bv_version)
-    #elif bv.init_status == id003.POW_UP_BIA:
-    #    print("BV powered up with bill in acceptor.")
-    #elif bv.init_status == id003.POW_UP_BIS:
-    #    print("BV powered up with bill in stacker.")
+    if bv.init_status == id003.POW_UP:
+        print("BV powered up normally, version:\n\t" + bv.bv_version)
+    elif bv.init_status == id003.POW_UP_BIA:
+        print("BV powered up with bill in acceptor.")
+    elif bv.init_status == id003.POW_UP_BIS:
+        print("BV powered up with bill in stacker.")
         
     while True:
-        try:
-            status, data = bv.req_status()
-        except:
-            print('Exception')
-        
-        #print(hex(status) + ' ' + data.decode())
+        status, data = bv.req_status()
         
         time.sleep(0.2)
 
