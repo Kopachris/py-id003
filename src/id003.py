8ffc38cbd1eba2873ea57ada3dee0c0a549cc710
Christopher Koch <kopachris@gmail.com>
2015-12-20 04:16:04 +0000

    set up logging

--- src/id003.py
+++ src/id003.py
@@ -2,6 +2,7 @@
 
 import serial
 import time
+import logging
 
 
 ###
@@ -297,7 +298,7 @@ def get_crc(message):
 class BillVal:
     """Represent an ID-003 bill validator as a subclass of `serial.Serial`"""
     
-    def __init__(self, port):
+    def __init__(self, port, log_raw=False):
         self.com = serial.Serial(port, 9600, serial.EIGHTBITS, serial.PARITY_EVEN, timeout=0.05)
         
         self.bv_status = None
@@ -328,43 +329,63 @@ class BillVal:
         
         # TODO get this from version during powerup
         self.bv_denoms = ESCROW_USA
+        
+        # set up logging
+        self.raw = log_raw
+        
+        logging.basicConfig(level=logging.DEBUG,
+                            format="[%(asctime)s] %(levelname)s: %(message)s",
+                            filename='debug.log',
+                            filemode='w',
+                            )
+        console = logging.StreamHandler()
+        console.setLevel(logging.INFO)
+        formatter = logging.Formatter("%levelname)s: %(message)s")
+        console.setFormatter(formatter)
+        logging.getLogger('').addHandler(console)
+    
+    def _raw(self, pre, msg):
+        if self.raw:
+            log = open('raw.log', 'w')
+            log.write('%s %s\r\n' % pre, msg)
+            log.close()
     
     def _on_stacker_full(self, data):
-        print("Stacker full.")
+        logging.error("Stacker full.")
     
     def _on_stacker_open(self, data):
-        print("Stacker open.")
+        logging.warning("Stacker open.")
     
     def _on_acceptor_jam(self, data):
-        print("Acceptor jam.")
+        logging.error("Acceptor jam.")
     
     def _on_stacker_jam(self, data):
-        print("Stacker jam.")
+        logging.error("Stacker jam.")
     
     def _on_pause(self, data):
-        print("BV paused. If there's a second bill being inserted, remove it.")
+        logging.warning("BV paused. If there's a second bill being inserted, remove it.")
     
     def _on_cheated(self, data):
-        print("BV cheated.")
+        logging.warning("BV cheated.")
     
     def _on_failure(self, data):
         fault = ord(data)
         if fault not in FAILURE_CODES:
-            print("Unknown failure: %02x" % fault)
+            logging.error("Unknown failure: %02x" % fault)
         else:
-            print(FAILURE_CODES[fault])
+            logging.error(FAILURE_CODES[fault])
     
     def _on_comm_error(self, data):
-        print("Communication error.")
+        logging.warning("Communication error.")
     
     def _on_invalid_command(self, data):
-        print("Invalid command.")
+        logging.warning("Invalid command.")
     
     def _on_idle(self, data):
-        print("BV idle.")
+        logging.info("BV idle.")
     
     def _on_accepting(self, data):
-        print("BV accepting...")
+        logging.info("BV accepting...")
     
     def _on_escrow(self, data):
         escrow = data[0]
@@ -372,60 +393,58 @@ class BillVal:
             raise DenomError("Unknown denom in escrow: %x" % escrow)
         elif escrow == BARCODE_TKT:
             barcode = data[1:]
-            print("Barcode: %s" % barcode)
+            logging.info("Barcode: %s" % barcode)
         else:
-            print("Denom: %s" % self.bv_denoms[escrow])
+            logging.info("Denom: %s" % self.bv_denoms[escrow])
             
         s_r = ''
         while s_r not in ('s', 'r'):
             s_r = input("(S)tack or (R)eturn? ").lower()
             if s_r == 's':
-                print("Telling BV to stack...")
+                logging.info("Telling BV to stack...")
                 self.accepting_denom = self.bv_denoms[escrow]
                 status = None
                 while status != ACK:
                     self.send_command(STACK_1, b'')
                     status, data = self.read_response()
-                print("Received ACK")
+                logging.debug("Received ACK")
                 self.bv_status = None
-                return None
             elif s_r == 'r':
-                print("Telling BV to return...")
+                logging.info("Telling BV to return...")
                 status = None
                 while status != ACK:
                     self.send_command(RETURN, b'')
                     status, data = self.read_response()
-                print("Received ACK")
+                logging.debug("Received ACK")
                 self.bv_status = None
-                return None
                     
     
     def _on_stacking(self, data):
-        print("BV stacking...")
+        logging.info("BV stacking...")
     
     def _on_vend_valid(self, data):
-        print("Vend valid for %s." % self.accepting_denom)
+        logging.info("Vend valid for %s." % self.accepting_denom)
         self.send_command(ACK, b'')
         self.accepting_denom = None
     
     def _on_stacked(self, data):
-        print("Stacked.")
+        logging.info("Stacked.")
 
     def _on_rejecting(self, data):
         reason = ord(data)
         if reason in REJECT_REASONS:
-            print("BV rejecting, reason: %s" % REJECT_REASONS[reason])
+            logging.warning("BV rejecting, reason: %s" % REJECT_REASONS[reason])
         else:
-            print("BV rejecting, unknown reason: %02x" % reason)
+            logging.warning("BV rejecting, unknown reason: %02x" % reason)
     
     def _on_returning(self, data):
-        print("BV Returning...")
+        logging.info("BV Returning...")
     
     def _on_holding(self, data):
-        print("Holding...")
+        logging.info("Holding...")
     
     def _on_inhibit(self, data):
-        print("BV inhibited.")
+        logging.warning("BV inhibited.")
         input("Press enter to reset and initialize BV.")
         status = None
         while status != ACK:
@@ -436,7 +455,7 @@ class BillVal:
         self.bv_status = None
     
     def _on_init(self, data):
-        print("BV waiting for initialization")
+        logging.warning("BV waiting for initialization")
         input("Press enter to reinitialize the BV.")
         self.initialize()
     
@@ -446,6 +465,9 @@ class BillVal:
         length = 5 + len(data)  # SYNC, length, command, and 16-bit CRC
         message = bytes([SYNC, length, command]) + data
         message += get_crc(message)
+        
+        # log message
+        self._raw('>', message)
         
         return self.com.write(message)
         
@@ -475,8 +497,11 @@ class BillVal:
             
         crc = self.com.read(2)
         
-        # check all our data...
+        # log message
         full_msg = start + total_length + command + data
+        self._raw('<', full_msg)
+        
+        # check our data
         if get_crc(full_msg) != crc:
             raise CRCError("CRC mismatch")
             
@@ -496,13 +521,14 @@ class BillVal:
         if status not in (POW_UP, POW_UP_BIA, POW_UP_BIS):
             raise PowerUpError("Acceptor already powered up")
         elif status == POW_UP:
-            print("Powering up...\nGetting version...")
+            logging.info("Powering up...")
+            logging.info("Getting version...")
             self.send_command(GET_VERSION)
             status, self.bv_version = self.read_response()
             print(self.bv_version)
             
             while status != ACK:
-                print("Sending reset command")
+                logging.debug("Sending reset command")
                 self.send_command(RESET)
                 status, data = self.read_response()
                 
@@ -528,36 +554,42 @@ class BillVal:
                    inhibit=[0], bar_func=[0x01, 0x12], bar_inhibit=[0]):
         """Initialize BV settings"""
         
+        logging.debug("Setting denom inhibit: %r" % denom)
         denom = bytes(denom)
         self.send_command(SET_DENOM, denom)
         status, data = self.read_response()
         if (status, data) != (SET_DENOM, denom):
             raise AckError("Acceptor did not echo denom settings")
         
+        logging.debug("Setting security: %r" % security)
         security = bytes(security)
         self.send_command(SET_SECURITY, security)
         status, data = self.read_response()
         if (status, data) != (SET_SECURITY, security):
             raise AckError("Acceptor did not echo security settings")
             
+        logging.debug("Setting optional functions: %r" % opt_func)
         opt_func = bytes(opt_func)
         self.send_command(SET_OPT_FUNC, opt_func)
         status, data = self.read_response()
         if (status, data) != (SET_OPT_FUNC, opt_func):
             raise AckError("Acceptor did not echo option function settings")
             
+        logging.debug("Setting inhibit: %r" % inhibit)
         inhibit = bytes(inhibit)
         self.send_command(SET_INHIBIT, inhibit)
         status, data = self.read_response()
         if (status, data) != (SET_INHIBIT, inhibit):
             raise AckError("Acceptor did not echo inhibit settings")
         
+        logging.debug("Setting barcode functions: %r" % bar_func)
         bar_func = bytes(bar_func)
         self.send_command(SET_BAR_FUNC, bar_func)
         status, data = self.read_response()
         if (status, data) != (SET_BAR_FUNC, bar_func):
             raise AckError("Acceptor did not echo barcode settings")
 
+        logging.debug("Setting barcode inhibit: %r" % bar_inhibit)
         bar_inhibit = bytes(bar_inhibit)
         self.send_command(SET_BAR_INHIBIT, bar_inhibit)
         status, data = self.read_response()
@@ -569,13 +601,13 @@ class BillVal:
         
         if self.com.in_waiting:
             # discard any unused data
-            print("WARNING: found unused data in buffer, %r" % self.com.read(self.com.in_waiting))
+            logging.warning("Found unused data in buffer, %r" % self.com.read(self.com.in_waiting))
             
         self.send_command(STATUS_REQ)
         
         stat, data = self.read_response()
         if stat not in NORM_STATUSES + ERROR_STATUSES + POW_STATUSES:
-            print("Unknown status code received: %02x, data: %s" % stat, data)
+            logging.warning("Unknown status code received: %02x, data: %r" % stat, data)
         
         return stat, data
         
--- test.py
+++ test.py
@@ -5,6 +5,7 @@ import id003
 import serial.tools.list_ports
 import serial
 import time
+import logging
 
 
 def main():
@@ -15,11 +16,11 @@ def main():
     bv.power_on()
     
     if bv.init_status == id003.POW_UP:
-        print("BV powered up normally.")
+        logging.info("BV powered up normally.")
     elif bv.init_status == id003.POW_UP_BIA:
-        print("BV powered up with bill in acceptor.")
+        logging.info("BV powered up with bill in acceptor.")
     elif bv.init_status == id003.POW_UP_BIS:
-        print("BV powered up with bill in stacker.")
+        logging.info("BV powered up with bill in stacker.")
         
     bv.poll()
 
