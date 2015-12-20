7a1a5987ea10be870f0a22be9ac77153526b640b
Christopher Koch <kopachris@gmail.com>
2015-12-20 22:53:31 +0000

    added com port select menu

--- protocol_analyzer.py
+++ protocol_analyzer.py
@@ -11,10 +11,15 @@ import id003
 import termutils as t
 
 import time
+import logging
+import msvcrt
+
+import serial.tools.list_ports
 
 from collections import OrderedDict
 
 X_SIZE, Y_SIZE = t.get_size()
+COMPORT = 'COM3'
 
 
 def poll_loop(bv, interval):
@@ -26,6 +31,10 @@ def poll_loop(bv, interval):
                 bv.bv_events[status](data)
         bv.bv_status = (status, data)
         wait = interval - (time.time() - poll_start)
+        if msvcrt.kbhit():
+            k = msvcrt.getch()
+            if k == 'q':
+                sys.exit()
         if wait > 0.0:
             time.sleep(wait)
             
@@ -62,20 +71,55 @@ def display_menu(menu, prompt='>>>', header='', info=''):
 
 
 def main():
+    global COMPORT
+
     main_menu = OrderedDict()
     main_menu['r'] = "Run"
     main_menu['s'] = "Settings"
+    main_menu['c'] = "Select COM port"
+    main_menu['q'] = "Quit"
     
-    choice = display_menu(main_menu, '>>>', "ID-003 protocol analyzer", "Using COM port COM11").lower()
+    choice = display_menu(main_menu, '>>>', "ID-003 protocol analyzer", "Using COM port %s" % COMPORT).lower()
     
     if choice == 'r':
-        import test
         t.wipe()
-        test.main()
+        bv = id003.BillVal(COMPORT)
+        print("Please connect bill validator.")
+        bv.power_on()
+        
+        if bv.init_status == id003.POW_UP:
+            logging.info("BV powered up normally.")
+        elif bv.init_status == id003.POW_UP_BIA:
+            logging.info("BV powered up with bill in acceptor.")
+        elif bv.init_status == id003.POW_UP_BIS:
+            logging.info("BV powered up with bill in stacker.")
+            
+        print("Press Q at any time to quit")
+        poll_loop(bv, 0.2)
+        return
     elif choice == 's':
         t.wipe()
         print("Settings not available yet")
+        input("Press enter to continue.")
+        return
+    elif choice == 'c':
+        t.wipe()
+        com_menu = OrderedDict()
+        ports = list(serial.tools.list_ports.comports())
+        for i, p in enumerate(ports):
+            com_menu[str(i+1)] = p.description
+        com_menu['q'] = "Back to main menu"
+        port = display_menu(com_menu, '>>>', "Select COM port")
+        if port == 'q':
+            return
+        else:
+            port = int(port) - 1
+            COMPORT = ports[port].device
+        return
+    elif choice == 'q':
+        sys.exit()
 	
 	
 if __name__ == '__main__':
-    main()
+    while 1:
+        main()
